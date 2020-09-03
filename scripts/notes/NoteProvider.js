ca38acb99415bd6957f0a09f3ae519d9eb4fc26c
Brenda Long <brenda@studio6thave.com>
2020-09-03 13:48:11 -0500

    more notes

--- scripts/notes/NoteProvider.js
+++ scripts/notes/NoteProvider.js
@@ -5,6 +5,28 @@ get all the notes from DB
 add a note to the DB
 */
 
+let notes = [];
+
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+const getNotes = () => {
+    return fetch('http://localhost:8088/notes')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+
+}
+
+
+const getNotes = () => {
+	console.log("getNotes called");
+}
+
 export const saveNote = noteObj => {
 	return fetch("http://localhost:8088/notes", {
 		method: "POST",
@@ -13,7 +35,8 @@ export const saveNote = noteObj => {
 		},
 		body: JSON.stringify(noteObj)
 	})
-	.then((result) => {
-		console.log("CELEBRATE RIGHT NOW");
+	.then(() => {
+		getNotes()
 	})
+    // .then(dispatchStateChangeEvent)
 }
