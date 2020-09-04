a3ac2abb01b008aefcc0967bdfe8239c52ef2fe4
Brenda Long <brenda@studio6thave.com>
2020-09-03 22:06:39 -0500

    added alibi display

---
+++ scripts/criminals/AlibiDialog.js
@@ -0,0 +1,34 @@
+import {useCriminals} from "./CriminalProvider.js"
+
+//get a reference to eventHub
+const eventHub = document.querySelector("#main");
+
+eventHub.addEventListener("alibiClicked", customEvent => {
+	const criminalId = customEvent.detail.chosenCriminal
+	const contentTarget = document.querySelector(`.alibiDialog--${criminalId}`)
+    
+
+    const targetCriminal = useCriminals().find(
+        (criminal) => criminal.id === parseInt(criminalId)
+    )
+
+    contentTarget.innerHTML = `${
+        targetCriminal.known_associates.map(associate => {
+            return `
+                <h4>${associate.name}</h4>
+                <div>${associate.alibi}</div>
+            `
+        }).join("")
+    }`
+
+    // Show the dialog element
+    // contentTarget.showModal() or show()
+})
+
+
+export const AlibiDialog = (id) => {
+	return `
+		<p class="alibiDialog--${id}">
+		
+        </p>`
+}
--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,10 +1,32 @@
+//setup a listener for the alibi button
+import {AlibiDialog} from './AlibiDialog.js'
+const eventHub = document.querySelector("#main");
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("alibi--")) {
+        const [prefix, criminalId] = clickEvent.target.id.split("--")
+
+        const alibiEvent = new CustomEvent("alibiClicked", {
+            detail: {
+                chosenCriminal: criminalId
+            }
+        })
+
+        eventHub.dispatchEvent(alibiEvent)
+    }
+})
+
+
+
 export const CriminalHTML = (criminalObj) => {
 	return `
-		<section id="officer-${criminalObj.id}" class="card-criminal">
+		<section id="criminal-${criminalObj.id}" class="card-criminal">
 			<p><strong>Name:</strong> ${criminalObj.name}</p>
 			<p>Age: ${criminalObj.age}</p>
 			<p>Crime: ${criminalObj.conviction}</p>
 			<p>Dates:</p>
+			<button id="alibi--${criminalObj.id}">Alibis</button>
+			${AlibiDialog(criminalObj.id)}
 		</section>
 	`
 } 
