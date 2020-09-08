27fd3d4d6bd743f56ade9b0b3821fbff1aaaab5c
Brenda Long <brenda@studio6thave.com>
2020-09-08 10:42:15 -0500

    added alibis associate

---
+++ scripts/criminals/AlibiDialog.js
@@ -0,0 +1,34 @@
+import {useCriminals} from "./CriminalProvider.js";
+
+const eventHub = document.querySelector("#main")
+
+eventHub.addEventListener("associatesClicked", event => {
+	//display all associates for criminal
+	
+	const smoothCriminal = useCriminals().find(criminal => {
+		return criminal.id === parseInt(event.detail.chosenCriminal)
+	})
+
+	const alibiTarget = document.querySelector(`.alibiDialog--${smoothCriminal.id}`);
+	const hTarget = document.querySelector("h4")
+
+	if (alibiTarget.contains(hTarget)){
+		alibiTarget.innerHTML = ""
+	}else {
+		alibiTarget.innerHTML = `${
+			smoothCriminal.known_associates.map(associate => {
+				return `
+				<h4>${associate.name}</h4>
+				<div>${associate.alibi}</div>
+				`
+			}).join("")
+		}`
+	}
+
+})
+
+export const AlibiDialog = (id) => {
+	return `
+		<span class="alibiDialog--${id}"></span>
+	`
+}
--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,3 +1,22 @@
+import {AlibiDialog} from './AlibiDialog.js';
+
+const eventHub = document.querySelector("#main");
+
+eventHub.addEventListener("click", event => {
+	if(event.target.id.startsWith("associates--")){
+		const [prefix, criminalId] = event.target.id.split("--")
+
+		const alibiEvent = new CustomEvent("associatesClicked", {
+			detail: {
+				chosenCriminal: criminalId
+			}
+		})
+
+		eventHub.dispatchEvent(alibiEvent);
+	}
+})
+
+
 export const CriminalHTML = (criminalObj) => {
 	return `
 		<section id="officer-${criminalObj.id}" class="card-criminal">
@@ -5,6 +24,8 @@ export const CriminalHTML = (criminalObj) => {
 			<p>Age: ${criminalObj.age}</p>
 			<p>Crime: ${criminalObj.conviction}</p>
 			<p>Dates:</p>
+			<button id="associates--${criminalObj.id}">Alibis</button>
+			${AlibiDialog(criminalObj.id)}
 		</section>
 	`
 } 
